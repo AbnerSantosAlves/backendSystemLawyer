a95dbc77e2ddc9f52ca68b8ccf4b5c96c81ea483
AbnerSantosAlves <abneralves562@gmail.com>
2026-09-17 15:56:51 -0300

    Configurando CORS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from database.session import Base, engine
 from routers.webhook_router import router as webhook_router
 
@@ -7,6 +7,15 @@ from routers.webhook_router import router as webhook_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.api_route("/", methods=["GET", "HEAD"])
